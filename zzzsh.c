e0d4f57126d6d2869f0424f88f2357e25efcfa03
jaredy <>
2004-06-06 05:29:18 +0000

    use execve instead of fork, to avoid major security vulns; add env vars to child; add char processing to allow execve

--- zzzsh.c
+++ zzzsh.c
@@ -10,25 +10,26 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
-#include <err.h>
 #include <sysexits.h>
+#include <err.h>
 
 void execute(char *);
 
 struct allowprog {
-	char *name;
-	char *path;
+	char *ap_name;
+	char *ap_path;
 } allowed[] = {
-	{"scp",			"/usr/bin/scp"  },
-	{"sftp",		"/usr/bin/sftp" },
-	{"cvs",			"/usr/bin/cvs"  },
-	{"/usr/bin/scp",	"/usr/bin/scp"  },
-	{"/usr/bin/sftp",	"/usr/bin/sftp" },
-	{"/usr/bin/cvs",	"/usr/bin/cvs"  },
+	{ "cvs",		"/usr/bin/cvs" },
+	{ "/usr/bin/cvs",	"/usr/bin/cvs" },
 	{ NULL, NULL }
 };
 
-int main(int argc, char *argv[])
+char *nenvp[] = {
+	NULL
+};
+
+int
+main(int argc, char *argv[])
 {
 	char buf[BUFSIZ+1], ch, **t;
 	extern char *optarg;
@@ -41,7 +42,7 @@ int main(int argc, char *argv[])
 					strlcpy(buf, optarg+2, sizeof buf);
 					strlcat(buf, " ", sizeof buf);
 				}
-				/* traverse subsequent arguments */
+				/* Traverse subsequent arguments. */
 				for (t = argv + optind; *t != NULL && strlen(buf) < BUFSIZ; t++) {
 					strlcat(buf, *t, sizeof buf);
 					if (t[1] != NULL)
@@ -76,15 +77,55 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-void execute(char *cmd)
+void
+execute(char *cmd)
 {
-	char *s;
+	char *s, **argv;
 	struct allowprog *p;
+	int len, pos;
 
-	for (p = allowed; p->name != NULL; p++) {
-		if (strncmp(p->name, cmd, strlen(p->name)) == 0) {
-			system(cmd);
-			return;
+	while ((*cmd == ' ' || *cmd == '\t' || *cmd == '\n') &&
+	       *cmd != '\0')
+		cmd++;
+
+	if (*cmd == '\0')
+		return;
+
+	for (p = allowed; p->ap_name != NULL; p++) {
+		pos = strlen(p->ap_name);
+		if (strncmp(p->ap_name, cmd, strlen(p->ap_name)) == 0 &&
+		    (cmd[pos] == ' ' || cmd[pos] == '\0')) {
+			switch (fork()) {
+			case -1:
+				warnx("fork");
+				break;
+			case 0:
+				/* XXX: quotes, escape, etc. */
+				for (s = cmd, len = 1;
+				     (s = strpbrk(s, " \t\r\n")) != NULL;
+				     len++, s++)
+					;
+
+				if ((argv = malloc(len * sizeof(char *))) == NULL)
+					err(EX_OSERR, "malloc");
+
+				for (pos = 1, argv[0] = cmd;
+				     (s = strpbrk(cmd, " \t\r\n")) != NULL;
+				     pos++) {
+					*s = '\0';
+					argv[pos++] = s + 1;
+				}
+
+				argv[pos] = NULL;
+
+				execve(p->ap_path, argv, nenvp);
+				err(EX_OSERR, "exec");
+				/* NOTREACHED */
+			default:
+				wait((int *)NULL);
+				return;
+				/* NOTREACHED */
+			}
 		}
 	}
 	if ((s = strpbrk(cmd, " \t\r\n")) != NULL)
